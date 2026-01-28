4537a19df3bd0ec697bde8aef626754c060ae544
Nedajist <Kaimeng44@proton.me>
2026-01-28 02:04:51 -0800

    finishing touches

--- HW4/Assets/Scripts/Bird.cs
+++ HW4/Assets/Scripts/Bird.cs
@@ -16,7 +16,6 @@ public class Bird : MonoBehaviour
     public event BirdDelegate BirdJumped;
     public event BirdDelegate BirdDied;
 
-    private float _rotationCount = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -35,19 +34,10 @@ public class Bird : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             _rigidbody2D.velocity = (Vector2.up * _jumpHeight);
-            _rotationCount += 0.2f;
             bird_sprite.color = new Color(Random.Range(0, 1.0f), Random.Range(0, 1.0f), Random.Range(0, 1.0f));
             BirdJumped.Invoke();
         }
-        if (_rotationCount > 0)
-        {
-            transform.Rotate(Vector3.forward * 0.5f);
-            _rotationCount -= Time.deltaTime;
-        }
-        else
-        {
-            _rotationCount = 0; 
-        }
+     
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
